de78e596e35b517828ed135015033b40934323a4
Anna Clifton <annacat@live.unc.edu>
2022-01-22 14:04:02 -0500

    Progress on Exercise 1

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,46 @@
+"""Ex01 - Chardle - A cute step toward Wordle."""
+
+__author__ = 730310234
+
+five_letter_word: str = str(input("Enter a 5-character word: "))
+
+if len(five_letter_word) != 5:
+    exit("Error: Word must contain 5 characters")
+else:
+
+    single_character: str = str(input("Enter a single character: "))
+    count: int = 0
+
+    if len(single_character) != 1:
+        exit("Error: Character must be a single character")
+    else:
+
+        print("Searching for " + single_character + " in " + five_letter_word)
+
+        if single_character == (five_letter_word)[0]:
+            print(single_character + " found at index 0 ")
+            count = count + 1
+
+        if single_character == (five_letter_word)[1]:
+            print(single_character + " found at index 1 ")
+            count = count + 1
+
+        if single_character == (five_letter_word)[2]:
+            print(single_character + " found at index 2 ")
+            count = count + 1
+
+        if single_character == (five_letter_word)[3]:
+            print(single_character + " found at index 3 ")
+            count = count + 1
+
+        if single_character == (five_letter_word)[4]:
+            print(single_character + " found at index 4 ")
+            count = count + 1
+
+        if count == 0:
+            print("No instances of " + single_character + " in " + five_letter_word)
+        else:
+            if count == 1:
+                print("1 instance of " + single_character + " in " + five_letter_word)
+            else:
+                print(count, "instances of " + single_character + " in " + five_letter_word)
